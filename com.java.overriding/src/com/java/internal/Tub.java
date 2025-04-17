eb1627c080c8ce0a2daede1ef9f2e1e437e65d54
Lavanya-Shivu <lavanyav0215@gmail.com>
2025-04-17 13:10:52 +0530

    Commit

--- com.java.overriding/src/com/java/internal/Tub.java
+++ com.java.overriding/src/com/java/internal/Tub.java
@@ -1,7 +1,27 @@
 package com.java.internal;
 
 public class Tub {
+    private String material;
+    private int capacity;
+
+    public Tub(String material, int capacity) {
+        this.material = material;
+        this.capacity = capacity;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
     public void fill() {
         System.out.println("Filling the tub with water.");
+    }
+
+    @Override
+    public String toString() {
+        return "Tub [Material: " + material + ", Capacity: " + capacity + " liters]";
     }
 }
--- com.java.overriding/src/com/java/internal/Tunnel.java
+++ com.java.overriding/src/com/java/internal/Tunnel.java
@@ -1,7 +1,27 @@
 package com.java.internal;
 
 public class Tunnel {
+    private String location;
+    private int length;
+
+    public Tunnel(String location, int length) {
+        this.location = location;
+        this.length = length;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
     public void enter() {
         System.out.println("Entering the tunnel...");
+    }
+    @Override
+    public String toString() {
+        return "Tunnel [Location: " + location + ", Length: " + length + " meters]";
     }
 }
