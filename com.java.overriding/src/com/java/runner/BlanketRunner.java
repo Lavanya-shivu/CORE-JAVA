7d993c08e23fff97c8da01a9273af75b7b284882
Lavanya-Shivu <lavanyav0215@gmail.com>
2025-04-18 12:59:42 +0530

    Commit

--- com.java.overriding/src/com/java/runner/BlanketRunner.java
+++ com.java.overriding/src/com/java/runner/BlanketRunner.java
@@ -1,9 +1,13 @@
 package com.java.runner;
 
 import com.java.external.Airconditioner.WoolenBlanket;
+import com.java.internal.Blanket;
 
 public class BlanketRunner {
     public static void main(String[] args) {
+        Blanket blanket1 = new Blanket("woolen","L");
+        Blanket balanket2 = new Blanket();
+
         WoolenBlanket blanket = new WoolenBlanket();
         blanket.cover();
         System.out.println("--------------------------");
