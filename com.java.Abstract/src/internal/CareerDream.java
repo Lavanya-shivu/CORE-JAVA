11796aaae557ed23345d702e135b5b275c7e83d7
Lavanya-Shivu <lavanyav0215@gmail.com>
2025-04-20 12:20:01 +0530

    Commit

---
+++ com.java.Abstract/src/internal/CareerDream.java
@@ -0,0 +1,26 @@
+package internal;
+
+public class CareerDream extends Dream{
+
+    @Override
+    public void imagine(){
+        System.out.println("Imaging a successfull career");
+    }
+    @Override
+    public void pursue(){
+        System.out.println("Pursuing an IT Job");
+    }
+    @Override
+    public void struggle(){
+        System.out.println("Struggle to pursue a job");
+    }
+    @Override
+    public void succeed(){
+        System.out.println("Succeeding in life");
+    }
+    @Override
+    public void reflect(){
+        System.out.println("Reflecting the growth");
+    }
+
+}
